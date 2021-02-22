2c66379c7fadfeb733f04e1f6ef4b71b13144680
Raimo Jämsén <62938861+Raimo78@users.noreply.github.com>
2021-02-22 10:33:31 +0200

    comments-delete.php
    I test now my comments-delete.php

---
+++ comment-delete.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'connect.php';
+
+$comment_id = $_POST['comment_id'];
+
+$sql_del = "DELETE FROM tbl_user_comments WHERE id = $comment_id";
+$stmt = $conn->prepare($sql_del);
+$stmt->execute();
+
+if (! empty($stmt)) {
+    echo true;
+}
+
+?>
